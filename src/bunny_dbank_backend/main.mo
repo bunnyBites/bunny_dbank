f5c034013fdda5b371aba264ea8cc1fcc1d75c35
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-12 01:16:44 +0530

    add starting files

--- src/bunny_dbank_backend/main.mo
+++ src/bunny_dbank_backend/main.mo
@@ -1,5 +1,41 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentAmount: Float = 300;
+  stable var startTime = Time.now();
+
+  public func getCurrentTime() {
+    Debug.print(debug_show(Time.now()))
+  };
+
+  public func topUp(amount : Float) {
+    currentAmount += amount;
+    Debug.print(debug_show (currentAmount));
+  };
+
+  public func withdrawAmount(amount : Float) {
+    let amountDiff: Float = (currentAmount - amount);
+
+    if (amountDiff <= 0) {
+      Debug.print("Not enough amount for transaction!");
+    } else {
+      currentAmount -= amount;
+      Debug.print(debug_show (currentAmount));
+    };
+  };
+
+  public query func getCurrentAmount(): async Float {
+    return currentAmount;
+  };
+
+  // compound = PrincipleAmount * (1 + (rateOfInterest / numberOfTimeInterestIsCompounded)) ^ (numberOfTimeInterestIsCompounded * totalTime)
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsed = Float.fromInt((currentTime - startTime) / (10 ** 9)); // nanosecond to secon
+
+    currentAmount := (currentAmount * (1.01 ** timeElapsed));
+    startTime := currentTime;
   };
 };
--- src/bunny_dbank_frontend/src/index.js
+++ src/bunny_dbank_frontend/src/index.js
@@ -1,19 +1,37 @@
 import { bunny_dbank_backend } from "../../declarations/bunny_dbank_backend";
 
+const fetchCurrentAmount = async () => {
+  const currentAmount = await bunny_dbank_backend.getCurrentAmount();
+
+  document.getElementById("value").innerHTML = currentAmount.toFixed(2);
+};
+
+window.addEventListener("load", fetchCurrentAmount);
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  const submitBtn = e.target.querySelector("#submit-btn");
 
-  const name = document.getElementById("name").value.toString();
+  const amountToTopUpInput = document.getElementById("input-amount");
+  const withdrawAmountInput = document.getElementById("withdrawal-amount");
 
-  button.setAttribute("disabled", true);
+  submitBtn.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await bunny_dbank_backend.greet(name);
+  if (!!amountToTopUpInput.value) {
+    await bunny_dbank_backend.topUp(Number.parseFloat(amountToTopUpInput.value));
+  }
 
-  button.removeAttribute("disabled");
+  if (!!withdrawAmountInput.value) {
+    await bunny_dbank_backend.withdrawAmount(Number.parseFloat(withdrawAmountInput.value));
+  }
 
-  document.getElementById("greeting").innerText = greeting;
+  await bunny_dbank_backend.compound();
+
+  fetchCurrentAmount();
+  amountToTopUpInput.value = "";
+  withdrawAmountInput.value = "";
+
+  submitBtn.removeAttribute("disabled");
 
   return false;
 });
